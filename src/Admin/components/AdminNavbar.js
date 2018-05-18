bb4ed70260e0ca727935b9e76814b007fd6066ef
Nar Cuenca <narc.ph@gmail.com>
2018-05-18 17:48:50 +0800

    adminnavbar fix

--- src/Admin/components/AdminNavbar.js
+++ src/Admin/components/AdminNavbar.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import propTypes from 'prop-types';
+
+import { authenticate } from '../../__utils/Authenticate';
+
+let styles = {}
+
+styles.button__logout = {
+  cursor: 'pointer',
+}
 
 const AdminNavbar = (props) => {
-  
+  const onLogout = () => {
+    authenticate.signout();
+    props.history.push('/admin/signin');
+  }
 
   return (
     <ul className="nav nav-tabs">
@@ -12,11 +24,15 @@ const AdminNavbar = (props) => {
       <li className="nav-item">
         <NavLink className="nav-link" activeClassName="active" to="/new">Add New</NavLink>
       </li>
-      
+      <li className="nav-item" style={styles.button__logout}>
+        <a onClick={() => onLogout()} className="nav-link btn btn-primary btn-sm mx-1">Logout</a>
+      </li>
     </ul>
   );
 }
 
-
+AdminNavbar.propTypes = {
+  history: propTypes.object
+}
 
 export default AdminNavbar;
