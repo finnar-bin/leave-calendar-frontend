f36a389e64a286f767f111907d426dbb5f07489e
Nar Cuenca <narc.ph@gmail.com>
2018-05-18 17:47:39 +0800

    added logout button

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,18 +1,21 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import AdminNavbar from './components/AdminNavbar';
 import Users from './components/Users';
 import New from './components/New';
+import AdminLogin from './AdminLogin';
 
-const Admin = () => {
+const Admin = (props) => {
   return (
     <div className="container">
       <Router>
         <Fragment>
-          <AdminNavbar />
+          <AdminNavbar {...props}/>
           <Switch>
             <Route path="/users" component={Users} />
-            <Route path="/new" component={New}/>
+            <Route path="/new" component={New} />
+            <Route path="/admin/signin" component={AdminLogin} />
           </Switch>
         </Fragment>
 
--- src/Admin/components/AdminNavbar.js
+++ src/Admin/components/AdminNavbar.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const AdminNavbar = () => {
+const AdminNavbar = (props) => {
+  
+
   return (
     <ul className="nav nav-tabs">
       <li className="nav-item">
@@ -10,11 +12,11 @@ const AdminNavbar = () => {
       <li className="nav-item">
         <NavLink className="nav-link" activeClassName="active" to="/new">Add New</NavLink>
       </li>
-      <li className="nav-item">
-        <NavLink className="nav-link" to="/logout">Logout</NavLink>
-      </li>
+      
     </ul>
   );
-};
+}
+
+
 
 export default AdminNavbar;
