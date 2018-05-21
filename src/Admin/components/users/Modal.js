119cedf9fd66e872b8b085d5d4a44d4af5df98e4
Nar Cuenca <narc.ph@gmail.com>
2018-05-21 16:50:43 +0800

    added user update modal with working update functionality

---
+++ src/Admin/components/users/Modal.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+import axios from 'axios';
+
+import './Modal.css';
+
+class Modal extends Component {
+  state = {
+    fullName: this.props.userInfo.fullName,
+    leaveCredits: Math.ceil(this.props.userInfo.leaveCredits*100)/100
+  }
+
+  modalClose = () => {
+    this.props.handleClose(true);
+  }
+
+  handleNameChange = (e) => {
+    this.setState({ fullName: e.target.value })
+  }
+
+  handleCreditsChange = (e) => {
+    this.setState({ leaveCredits: e.target.value })
+  }
+
+  handleSubmit = () => {
+    axios.patch(`${process.env.REACT_APP_API_GATEWAY}/user/${this.props.userInfo._id}`, {
+      fullName: this.state.fullName,
+      leaveCredits: this.state.leaveCredits
+    }, {
+      headers: {
+        'x-auth': localStorage.getItem('token')
+      }
+    }).then(response => console.log(response.data))
+    .catch(error => console.log(error.response.data.errorStack));
+  }
+  
+  render() {
+    console.log(this.props.userInfo);
+    return (
+      <div className="modal__overlay">
+        <div className="card p-3" style={{ minHeight: '10vh' }}>
+          <h4 className="card-header">Edit User</h4>
+          <div className="card-body">
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="fullname">Full Name</span>
+              </div>
+              <input
+                className="form-control"
+                type="text"
+                aria-describedby="username"
+                value={this.state.fullName}
+                onChange={this.handleNameChange}
+              />
+            </div>
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="leaveCredits">Leave Credits</span>
+              </div>
+              <input
+                className="form-control"
+                type="number"
+                aria-describedby="leaveCredits"
+                value={this.state.leaveCredits}
+                onChange={this.handleCreditsChange}
+              />
+            </div>
+            <div className="text-center">
+              <button className="btn btn-primary mx-1" onClick={this.handleSubmit}>Submit</button>
+              <button className="btn btn-warning mx-1" onClick={this.modalClose}>Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Modal.propTypes = {
+  handleClose: propTypes.func,
+  userInfo: propTypes.object
+}
+export default Modal;
--- src/Admin/components/users/Table.js
+++ src/Admin/components/users/Table.js
@@ -1,35 +1,70 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import propTypes from 'prop-types';
 
+import Modal from './Modal';
 
-const Table = (props) => {
-  let users = props.users.map((data) => {
-    return (
-      <tr key={data._id}>
-        <td>{data.fullName}</td>
-        <td>{data.leaveCredits}</td>
-        <td>
-          <button className="btn btn-outline-success btn-sm mx-1">Edit</button>
-          <button className="btn btn-outline-danger btn-sm mx-1">Remove</button>
-        </td>
-      </tr>
-    )
-  });
+class Table extends Component {
+  state = {
+    triggerModal: false,
+    toEdit: [],
+  }
 
-  return (
-    <table className="table table-hover text-center">
-      <thead className="thead-dark">
-        <tr>
-          <th>Name</th>
-          <th>Leave Credits</th>
-          <th>Actions</th>
+  onEdit = (userdata) => {
+    this.setState({
+      triggerModal: true,
+      toEdit: userdata
+    });
+  }
+
+  onRemove = (userid) => {
+    console.log('Remove', userid);
+  }
+
+  onClose = (bool) => {
+    this.setState({ triggerModal: bool })
+  }
+
+  render() {
+    let users = this.props.users.map((data) => {
+      return (
+        <tr key={data._id}>
+          <td>{data.fullName}</td>
+          <td>{Math.ceil(data.leaveCredits * 100)/100}</td>
+          <td>
+            <button
+              className="btn btn-outline-success btn-sm mx-1"
+              onClick={() => this.onEdit(data)}>
+              Edit
+            </button>
+            <button
+              className="btn btn-outline-danger btn-sm mx-1"
+              onClick={() => this.onRemove(data._id)}>
+              Remove
+            </button>
+          </td>
         </tr>
-      </thead>
-      <tbody>
-        {users}
-      </tbody>
-    </table>
-  );
+      )
+    });
+
+    return (
+      <Fragment>
+        <table className="table table-hover text-center">
+          <thead className="thead-dark">
+            <tr>
+              <th>Name</th>
+              <th>Leave Credits</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users}
+          </tbody>
+        </table>
+
+        {this.state.triggerModal && <Modal handleClose={() => this.onClose()} userInfo={this.state.toEdit}/>}
+      </Fragment>
+    );
+  }
 };
 
 Table.propTypes = {
