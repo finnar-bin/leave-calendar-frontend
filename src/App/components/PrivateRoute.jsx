9bb503a9cf12a4dbc666b30588ec4aa177746067
Nar Cuenca <narc.ph@gmail.com>
2018-07-09 14:59:41 +0800

    fixed routes for admin

--- src/App/components/PrivateRoute.jsx
+++ src/App/components/PrivateRoute.jsx
@@ -3,11 +3,10 @@ import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { authenticate } from '../utils/auth';
-import { isUser } from '../utils/user';
 
 const PrivateRoute = ({ component: Component, ...rest}) => (
   <Route {...rest} render={(props) => (
-    authenticate() && isUser() ? <Component {...props} /> : <Redirect to='/admin/signin' /> 
+    authenticate() ? <Component {...props} /> : <Redirect to='/admin/signin' /> 
   )} />
 )
 
