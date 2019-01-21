821247102bda7247070f5c50487cd88c92d32586
Nar Cuenca <narc.ph@gmail.com>
2019-01-21 17:28:13 +0800

    added leave request popup

--- src/components/Calendar/CalendarArea.js
+++ src/components/Calendar/CalendarArea.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import moment from "moment";
 import BigCalendar from "react-big-calendar";
@@ -6,6 +6,7 @@ import Paper from "@material-ui/core/Paper";
 import { withStyles } from "@material-ui/core/styles";
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import LeaveRequest from "./LeaveRequest";
 import { fetchHolidays, fetchLeaves } from "../../store/actions/calendarAction";
 
 // Big Calendar Date Localizer setup
@@ -22,23 +23,59 @@ const styles = theme => ({
 });
 
 class CalendarArea extends Component {
+  state = {
+    start: null,
+    end: null,
+    open: false
+  };
+
   componentDidMount() {
     this.props.fetchHolidays();
     this.props.fetchLeaves();
   }
 
+  handleSelectSlot = info => {
+    this.setState({
+      start: info.start.toLocaleDateString(),
+      end: info.end.toLocaleDateString(),
+      open: true
+    });
+    console.log(moment(this.state.start).format("YYYY-MM-DD"));
+  };
+
+  handleClose = () => {
+    this.setState({
+      start: null,
+      end: null,
+      open: false
+    });
+  };
+
   render() {
-    const { classes } = this.props;
+    const { classes, holidays, leaves } = this.props;
+    const { open, start, end } = this.state;
+
     return (
-      <Paper className={classes.root}>
-        <BigCalendar
-          className={classes.calendar}
-          localizer={localizer}
-          defaultDate={new Date()}
-          views={["month"]}
-          events={[...this.props.holidays.dates, ...this.props.leaves.dates]}
+      <Fragment>
+        <Paper className={classes.root}>
+          <BigCalendar
+            className={classes.calendar}
+            localizer={localizer}
+            defaultDate={new Date()}
+            views={["month"]}
+            events={[...holidays.dates, ...leaves.dates]}
+            selectable
+            popup
+            onSelectSlot={info => this.handleSelectSlot(info)}
+          />
+        </Paper>
+        <LeaveRequest
+          open={open}
+          handleClose={this.handleClose}
+          start={start}
+          end={end}
         />
-      </Paper>
+      </Fragment>
     );
   }
 }
